37c7507e5e7f9940c6e634a9aa1690e569d29c98
jordnlvr <kellydseale@msn.com>
2014-10-03 23:36:57 -0500

    added ice image

--- CS16/scripts/app.js
+++ CS16/scripts/app.js
@@ -30,12 +30,12 @@
 
 
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
+    document.addEventListener('deviceready', function () {
+
       // hide the splash screen as soon as the app is ready. otherwise
       // Cordova will wait 5 very long seconds to do it for you.
-      //navigator.splashscreen.hide();
-
+        //navigator.splashscreen.hide();
+      
       app = new kendo.mobile.Application(document.body, {
         
         // comment out the following line to get a UI which matches the look
